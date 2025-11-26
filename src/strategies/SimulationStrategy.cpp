508d577699cb75f036ef00e126f6f2c67962827a
StefieS <mikilesiga@gmail.com>
2025-11-26 21:06:31 +0100

    feat: activation of processes

--- src/strategies/SimulationStrategy.cpp
+++ src/strategies/SimulationStrategy.cpp
@@ -4,12 +4,14 @@
 // TODO: Define all shared functions
 
 void SimulationStrategy::beeHivesProcess() {
+
     for (int i = 0; i < hives_to_open; i++) {
         new OpeningHive();
     }
+
     processMap["LoadingFromStandToTransport"] = new LoadingFromStandToTransport();
-
+    processMap["ReturningEmptyFramesToHive"] = new ReturningEmptyFramesToHive();
+    processMap["LoadingFromStandToTransport"]->activate;
+    processMap["ReturningEmptyFramesToHive"]->activate;
     
-
-
 }
