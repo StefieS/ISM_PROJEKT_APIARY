2a324484ba74a58816a1f1547bf9b0fc3f31ba3d
StefieS <mikilesiga@gmail.com>
2025-11-26 23:53:09 +0100

    feat: shed beekeeper

--- src/strategies/OffSiteExtractionStrategy.cpp
+++ src/strategies/OffSiteExtractionStrategy.cpp
@@ -3,7 +3,7 @@
 
 void OffSiteExtractionStrategy::run() {
     //Transport = std::make_unique<Store>(OffSiteConstants::TRANSPORT_CAPACITY);
-    hiveBeekeeper = std::make_unique<Facility>("Off beekeeper");
-    
+    hiveBeekeeper = std::make_unique<Facility>("OFF_hivesBeekeeper");
+    shedBeekeeper = std::make_unique<Facility>("OFF_shedBeekeeper");
     
 }
